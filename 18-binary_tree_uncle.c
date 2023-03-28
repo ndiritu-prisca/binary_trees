7540510e2d9f28078826243be2068153ef106575
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-28 13:14:59 +0000

    Complete mandatory tasks

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+#include "binary_trees.h"
+
 /**
   * binary_tree_sibling - a function that finds the sibling of a node
   * @node: pointer to the node to find the sibling
@@ -19,4 +21,19 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (temp->right == node && temp->left != NULL)
 		return (temp->left);
 	return (NULL);
+}
+
+/**
+  * binary_tree_uncle - a function that finds the uncle of a node
+  * @node: pointer to the node to find the uncle
+  * Return: pointer to the uncle node or NULL
+  */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *tmp;
+
+	if (node == NULL)
+		return (NULL);
+	tmp = binary_tree_sibling(node->parent);
+	return (tmp);
 }
