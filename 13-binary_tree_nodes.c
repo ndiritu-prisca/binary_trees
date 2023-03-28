6c764882c5b9651806bc56bfdcf1ba305a56ccf5
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-28 18:03:00 +0000

    Corrections on 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,8 +12,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
 	if (tree->left != NULL && tree->right != NULL)
 		nodes = 1;
 	nodes = nodes + binary_tree_nodes(tree->left);
