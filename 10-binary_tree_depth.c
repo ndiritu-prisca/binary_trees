d9917987566d89421d9b9fab44536c995b2dc286
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-28 16:59:50 +0000

    Betty check on 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,5 +14,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	if (tree->parent != NULL)
 		depth = 1 + binary_tree_depth(tree->parent);
-	return (depth);	
+	return (depth);
 }
