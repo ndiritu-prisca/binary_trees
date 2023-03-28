936b8f66f7ea52e9b7df0800dac0cca38d4a697e
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-28 16:56:30 +0000

    Add 1st condition

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (parent == NULL)
+		return (NULL);
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
