0cfdba44bdf0c0e5c047567bbf8d62d8ba69a388
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-28 18:12:15 +0000

    Corrections on 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,7 +12,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	if (tree->left != NULL)
 		left_h = 1 + binary_tree_is_full(tree->left);
 	if (tree->right != NULL)
