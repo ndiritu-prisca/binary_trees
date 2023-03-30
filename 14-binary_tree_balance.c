c9ab79a89fc5d80e47b8e6d11fe7e505b863496e
ndiritu-prisca <priscandiritu@outlook.com>
2023-03-30 07:47:52 +0000

    Corrections on 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,19 +7,17 @@
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_h = 0, right_h = 0;
+	size_t left_h = 0, right_h = 0;
 
-        if (tree == NULL)
-                return (0);
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
-        if (tree->left != NULL)
-                left_h = 1 + binary_tree_height(tree->left);
-        if (tree->right != NULL)
-                right_h = 1 + binary_tree_height(tree->right);
-        if (left_h > right_h)
-                return (left_h);
-        return (right_h);
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		left_h = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right_h = 1 + binary_tree_height(tree->right);
+	if (left_h > right_h)
+		return (left_h);
+	return (right_h);
 }
 
 /**
@@ -37,11 +35,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree->left == NULL)
 		left_h = -1;
 	else
-		left_h = 1 + binary_tree_height(tree->left);
+		left_h = binary_tree_height(tree->left);
 	if (tree->right == NULL)
 		right_h = -1;
 	else
-		right_h = 1 + binary_tree_balance(tree->right);
+		right_h = binary_tree_balance(tree->right);
 	balance = left_h - right_h;
 
 	return (balance);
